98f18bf3a9aab08e2d3465993376c83bd729eed4
Jefferson <jeffabuy@gmail.com>
2025-02-19 09:27:09 +0200

    fixed hero picture

--- src/components/me/Me.jsx
+++ src/components/me/Me.jsx
@@ -1,10 +1,9 @@
+import profile from "/public/unnamed.jpg";
+
 const Me = () => {
   return (
     <div className="bg-opacity-20 w-[clamp(300px,60%,90%)] m-auto flex justify-center gap-4 px-20 py-10 rounded-md xl:w-full sm:px-0">
-      <img
-        className="rounded-full w-[clamp(100px,2vw,20%)]"
-        src={"/public/unnamed.jpg"}
-      />
+      <img className="rounded-full w-[clamp(100px,2vw,20%)]" src={profile} />
       <div className=" p-4 flex flex-col gap-4">
         <h1 className="text-title font-medium">Albert Jefferson Abuy</h1>
         <h2 className="text-subtitle px-4">
